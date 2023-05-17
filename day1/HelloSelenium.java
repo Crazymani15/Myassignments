e93f8e98e00ffb5e2ecff5cdbfb60886cf6674e3
Crazymani15 <85403388+Crazymani15@users.noreply.github.com>
2023-05-17 18:49:15 +0530

    Add files via upload
    week 1 day 1

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloSelenium {
+	
+	public static void main(String[] args) {
+		int age=25;
+		System.out.println(age);
+		System.out.println("started Learning automation, Thanks");
+	}
+
+}
---
+++ day1/Smartphone.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Smartphone {
+	public static void main(String[] args) {
+		Iphone m = new Iphone();
+		m.makecall();
+		m.model();
+		
+	}
+}
